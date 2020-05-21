87d4b172d62883a980c7f392f980fcfd3a3891c0
doozie96 <dooziescartoon@gmail.com>
2020-05-21 22:01:07 +0530

    sabe

---
+++ index.js
@@ -0,0 +1,18 @@
+//inputs
+var username=document.getElementsByName("username");
+var email=document.getElementsByName("email");
+var subject=document.getElementsByName("subject");
+var query=document.getElementsByName("query");
+const inputs=[username,email,subject,query];
+
+//labels
+var labelusername=document.querySelector(".labelusername");
+var labelemail=document.querySelector(".labelemail");
+var labelsubject=document.querySelector(".labelsubject");
+var labelquery=document.querySelector(".labelquery");
+const labels=[labelusername,labelemail,labelsubject,labelquery];
+
+function username1(){
+  lahelusername.style.top="50";
+}
+
