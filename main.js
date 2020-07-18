c045c7d8f0ecc110bc84bdee06a26a8c25755e6c
Doozie <dooziescartoon@gmail.com>
2020-07-18 19:07:16 +0530

    Save

---
+++ main.js
@@ -0,0 +1,10 @@
+function navopen() {
+  document.querySelector('.navbar').classList.toggle('activenavbar');
+  document.querySelector('.navicon1').classList.toggle('active1');
+  document.querySelector('.navicon2').classList.toggle('active2');
+  document.querySelector('.homepage').classList.toggle('activehomepage');
+}
+
+function nest() {
+  document.querySelector('.video').classList.toggle('activenest');
+}
